36047e0299a00d4ade4fdd20032a972bea0148d6
hadeel-farhan <hadeelf2001@gmail.com>
2023-03-30 10:33:07 -0400

    cors

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 TuitsController(app);
 HelloController(app)
