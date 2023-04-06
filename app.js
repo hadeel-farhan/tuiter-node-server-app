40832b3bf7f1ac723fed55d0dbdfe3a89a20bfcb
hadeel-farhan <hadeelf2001@gmail.com>
2023-04-06 14:19:33 -0400

    finished?

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
+import mongoose from "mongoose";
 
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -16,3 +18,6 @@ HelloController(app)
 UserController(app)
 
 app.listen(process.env.PORT || 4000);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
